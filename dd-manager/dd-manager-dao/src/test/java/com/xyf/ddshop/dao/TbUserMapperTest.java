702fe73e22dbe0f6130bae85428669e5450f4e35
Administrator <1908036707@qq.com>
2017-11-04 15:51:32 +0800

    反向工程搭建完毕，整合分布式成功，测试反向工程成功

---
+++ dd-manager/dd-manager-dao/src/test/java/com/xyf/ddshop/dao/TbUserMapperTest.java
@@ -0,0 +1,28 @@
+package com.xyf.ddshop.dao;
+
+import com.xyf.ddshop.pojo.po.TbUser;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * User: Administrator
+ * Date: 2017/11/4
+ * Time: 14:45
+ * Version:V1.0
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration({"classpath:spring/spring-dao-test.xml"})
+public class TbUserMapperTest {
+    @Autowired
+    private  TbUserMapper tbUserMapper;
+    @Test
+    public void testSelectByPrimaryKey() throws Exception {
+
+        TbUser tbUser = tbUserMapper.selectByPrimaryKey(5L);
+        System.out.println(tbUser.getUsername());
+    }
+
+}
